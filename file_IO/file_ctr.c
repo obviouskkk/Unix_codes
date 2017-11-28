dce97054444313e07de6dea6d65db1dc70cf1212
zzy <942744575@qq.com>
2017-11-28 21:37:47 +0800

    file and i/o

---
+++ file_IO/file_ctr.c
@@ -0,0 +1,22 @@
+/* ***********************************************************************
+
+  > File Name: file_ctr.c
+  > Author: zzy
+  > Mail: 942744575@qq.com 
+  > Created Time: Tue 28 Nov 2017 03:30:16 PM CST
+
+ ********************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, const char* argv[])
+{
+	int fd = open ("test.txt", O_CREAT|O_RDWR);
+	int w_len = write(fd, "hello world" ,11);
+	printf("%d\n",w_len);
+    return 0;
+}
+
