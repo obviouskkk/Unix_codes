93d79706f76d3a0598ecc28ff984a9899dd54ed6
zzy <942744575@qq.com>
2018-01-08 16:55:22 +0800

    sigmask

---
+++ signal/sigmask.c
@@ -0,0 +1,53 @@
+/* ***********************************************************************
+
+  > File Name: sigmask.c
+  > Author: zzy
+  > Mail: 942744575@qq.com 
+  > Created Time: Mon 08 Jan 2018 04:35:38 PM CST
+
+ ********************************************************************** */
+
+#include "../apue.h"
+
+void proc_mask(const char* str)
+{
+	sigset_t sigset;
+	int      errno_save;
+
+	errno_save = errno;
+	if (sigprocmask(0,NULL, &sigset) < 0)
+		err_ret("sigprocmask error");
+	else {
+		printf("%s", str);
+		if(sigismember(&sigset, SIGINT) )
+			printf("	SIGINT");
+		if(sigismember(&sigset, SIGQUIT) )
+			printf("	SIGQUIT");
+		if(sigismember(&sigset, SIGUSR1) )
+			printf("	SIGUER1");
+		if(sigismember(&sigset, SIGALRM) )
+			printf("	SIGUER1");
+		if(sigismember(&sigset, SIGHUP) )
+			printf("	SIGHUP");
+		printf("\n");
+	}
+
+}
+
+void sig_mask()
+{
+	sigset_t sigset, sigoset;
+	sigaddset(&sigset, SIGHUP);
+
+	if( sigprocmask(SIG_BLOCK, &sigset,&sigoset ) < 0 )
+		err_ret("sigprocmask error");
+}
+
+int main(int argc, const char* argv[])
+{
+	sig_mask();
+	proc_mask("hello");
+	sleep(10);
+    return 0;
+}
+
